2e309c58d98d09686e3ceaaa76e2bf2ddbb91d7e
kibxz <kibzy445@gmail.com>
2024-07-04 13:56:44 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,125 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Navbar from '../app/component/Navbar'; // Adjust the path according to your project structure
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <>
+      <Navbar />
+
+      <div className="container">
+        <div id="carouselExample" className="carousel slide p-5" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="/img/img1.jpg" className="d-block w-100" alt="Slide 1" />
+            </div>
+            <div className="carousel-item">
+              <img src="/img/img2.jpg" className="d-block w-100" alt="Slide 2" />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card mb-3">
+              <img src="/img/img3.jpg" className="custom-img" alt="Card Image 5" />
+              <div className="card-body text-center">
+                <h5 className="card-title">อนิเมะแนะนำ</h5>
+                <p className="card-text">
+                  Jujutsu Kaisen
+                </p>
+                <a href="#" className="btn btn-primary">Watch</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-3">
+              <img src="/img/img4.jpg" className="custom-img" alt="Card Image 6" />
+              <div className="card-body text-center">
+                <h5 className="card-title">อนิเมะแนะนำ</h5>
+                <p className="card-text">
+                  Kimitsuno Yaiba
+                </p>
+                <a href="#" className="btn btn-primary">Watch</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-3">
+              <img src="/img/img5.jpg" className="custom-img" alt="Card Image 7" />
+              <div className="card-body text-center">
+                <h5 className="card-title">อนิเมะแนะนำ</h5>
+                <p className="card-text">
+                  One piece
+                </p>
+                <a href="#" className="btn btn-primary">Watch</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Footer />
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="py-5 container">
+      <div className="row">
+        <div className="col-6 col-md-2 mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Service</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contact</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div className="col-6 col-md-2 mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Service</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contact</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div className="col-6 col-md-2 mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Service</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contact</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div className="col-md-5 offset-md-1 mb-3">
+          <form>
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of what's new and exciting from us.</p>
+            <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+              <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
+              <button className="btn btn-primary" type="submit">Subscribe</button>
+            </div>
+          </form>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </footer>
   );
 }
